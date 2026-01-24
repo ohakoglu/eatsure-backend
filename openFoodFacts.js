a8b81487ad2b85efdef58f60be910c37ada8f0a3
ohakoglu <ohakoglu@gmail.com>
2026-01-25 01:48:39 +0300

    Add openFoodFacts.js

--- openFoodFacts.js
+++ openFoodFacts.js
@@ -1,1 +1,9 @@
+const axios = require("axios");
 
+async function fetchProductByBarcode(barcode) {
+  const url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`;
+  const response = await axios.get(url);
+  return response.data;
+}
+
+module.exports = { fetchProductByBarcode };
