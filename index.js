e2720a34d9a97fd3b1cad745d3d1b4a8888504ee
ohakoglu <ohakoglu@gmail.com>
2026-01-25 17:18:11 +0300

    Connect decision engine

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const { fetchProductByBarcode } = require("./openFoodFacts");
 const { analyzeGluten } = require("./glutenAnalyzer");
-
+const { decideGlutenStatus } = require("./decisionEngine");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -26,13 +26,18 @@ app.get("/scan/:barcode", async (req, res) => {
 
     const product = data.product;
     const analysis = analyzeGluten(product.ingredients_text);
-
+const decision = decideGlutenStatus({
+  certification: null, // şimdilik yok, birazdan ekleyeceğiz
+  ingredientAnalysis: analysis,
+  manufacturerClaim: analysis.claimsGlutenFree === true
+});
     res.json({
       barcode,
       name: product.product_name || "İsimsiz Ürün",
       brand: product.brands || "Bilinmiyor",
       ingredients: product.ingredients_text || null,
       analysis,
+      decision
     });
   } catch (error) {
     res.status(500).json({
