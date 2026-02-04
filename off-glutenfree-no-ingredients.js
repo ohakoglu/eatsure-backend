28b9a8981f31f814936bca69471b55b209f8caf0
ohakoglu <ohakoglu@gmail.com>
2026-02-04 21:17:16 +0000

    Update off-glutenfree-no-ingredients.js

--- off-glutenfree-no-ingredients.js
+++ off-glutenfree-no-ingredients.js
@@ -1,1 +1,30 @@
-// temporary
+/**
+ * OFF – Declared Gluten Free but NO INGREDIENTS
+ *
+ * Amaç:
+ * - OFF kaydı VAR
+ * - Ürün adında / etiket bilgisinde "gluten free / glutensiz" beyanı VAR
+ * - ingredients_text YOK
+ *
+ * Bu dosya SADECE test ve doğrulama içindir.
+ * Karar motoruna henüz bağlanmaz.
+ */
+
+module.exports = [
+  {
+    barcode: "8008698005070",
+    expected: {
+      offExists: true,
+      hasIngredients: false,
+      manufacturerClaim: true
+    }
+  },
+  {
+    barcode: "8008698004890",
+    expected: {
+      offExists: true,
+      hasIngredients: false,
+      manufacturerClaim: true
+    }
+  }
+];
